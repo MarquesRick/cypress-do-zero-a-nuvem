05b6969f542c0a1b6092cb30384cd8934f588315
MarquesRick <hmservicostech@outlook.com.br>
2026-01-19 19:47:50 -0300

    feat: used times() method to repeat a block or a function statement

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,9 +1,11 @@
 // Exercício 7: Teste Independente da Página de Política de Privacidade
-it('testa a página de política de privacidade de forma independente', () => {
-    cy.visit('./src/privacy.html');
+Cypress._.times(3, () => {
+    it('testa a página de política de privacidade de forma independente', () => {
+        cy.visit('./src/privacy.html');
 
-    cy.contains('h1', 'CAC TAT - Política de Privacidade')
-        .should('be.visible');
-    cy.contains('p', 'Talking About Testing').should('be.visible');
-    cy.get('p').should('have.length.greaterThan', 0);
+        cy.contains('h1', 'CAC TAT - Política de Privacidade')
+            .should('be.visible');
+        cy.contains('p', 'Talking About Testing').should('be.visible');
+        cy.get('p').should('have.length.greaterThan', 0);
+    });
 });
